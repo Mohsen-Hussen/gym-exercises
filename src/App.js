d6a0fccb58e57167d81ef7407e0f63ca1ca8f902
mohsen-hussen <mohsenhussen8@gmail.com>
2022-07-16 17:47:30 +0200

    File & Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/exercise/:id" element={<ExerciseDetail />} />
+			</Routes>
+			<Footer />
+		</Box>
+	);
 };
 
 export default App;
