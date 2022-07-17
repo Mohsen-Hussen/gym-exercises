3587401546bea7cbdb88f48bb60ea64b3da8a439
mohsen-hussen <mohsenhussen8@gmail.com>
2022-07-17 03:12:38 +0200

    add Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<Box>
+			<HeroBanner />
+			<SearchExercises />
+			<Exercises />
+		</Box>
+	);
 };
 
 export default Home;
