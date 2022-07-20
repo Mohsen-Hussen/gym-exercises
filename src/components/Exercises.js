05594aec0e1bcb37ebc304fbb09a1fc8366712fe
mohsen-hussen <mohsenhussen8@gmail.com>
2022-07-20 17:05:53 +0200

    handle HorizontalScrollbar logic

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -25,6 +25,29 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 		window.scrollTo({ top: 1800, behavior: "smooth" });
 	};
 
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			let exercisesData = [];
+
+			if (bodyPart === "all") {
+				exercisesData = await fetchData(
+					"https://exercisedb.p.rapidapi.com/exercises",
+					exerciseOptions
+				);
+			} else {
+				exercisesData = await fetchData(
+					`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+					exerciseOptions
+				);
+			}
+
+			setExercises(exercisesData);
+		};
+
+		fetchExercisesData();
+		
+	}, [bodyPart]);
+
 	return (
 		<Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
 			<Typography
